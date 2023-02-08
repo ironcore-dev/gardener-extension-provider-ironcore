ed527942791541ee828a7dabcfac93ed209ac701
Kajol Asabe <kajol.asabe@t-systems.com>
2023-02-08 16:30:17 +0530

    added networkname in cloud-provider-config

--- pkg/onmetal/types.go
+++ pkg/onmetal/types.go
@@ -55,6 +55,10 @@ const (
 	KubeConfigFieldName = "kubeconfig"
 	// TokenFieldName is containing the token to access an onmetal cluster.
 	TokenFieldName = "token"
+	// NetworkName is the name of network
+	NetworkFieldName = "networkname"
+	// Kubeconfig is the config of kubernetes cluster where onmetal is running
+	KubeconfigFieldName = "kubeconfig"
 
 	// CloudControllerManagerName is a constant for the name of the CloudController deployed by the worker controller.
 	CloudControllerManagerName = "cloud-controller-manager"
