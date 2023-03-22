4e6bf86de0be58c6dba5d026ef5fb60e717dc27f
Andreas Fritzler <andreas.fritzler@sap.com>
2023-03-22 10:47:49 +0100

    Deprecate DeployMachineDependencies and CleanupMachineDependencies (#123)

--- pkg/controller/worker/machine_dependencies.go
+++ pkg/controller/worker/machine_dependencies.go
@@ -16,10 +16,14 @@ package worker
 
 import "context"
 
+// DeployMachineDependencies implements genericactuator.WorkerDelegate.
+// Deprecated: Do not use this func. It is deprecated in genericactuator.WorkerDelegate.
 func (w *workerDelegate) DeployMachineDependencies(_ context.Context) error {
 	return nil
 }
 
+// CleanupMachineDependencies implements genericactuator.WorkerDelegate.
+// Deprecated: Do not use this func. It is deprecated in genericactuator.WorkerDelegate.
 func (w *workerDelegate) CleanupMachineDependencies(ctx context.Context) error {
 	return nil
 }
