65413d947776209ca81d4cf543f70c5e9e4e2021
Damyan Yordanov <damyan.yordanov@sap.com>
2024-12-03 11:39:27 +0100

    Regenerate types

--- pkg/controller/infrastructure/actuator_reconcile.go
+++ pkg/controller/infrastructure/actuator_reconcile.go
@@ -6,13 +6,13 @@ package infrastructure
 import (
 	"context"
 	"fmt"
-	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"math/big"
 	"net"
 	"net/netip"
 	"slices"
 
 	"github.com/gardener/gardener/extensions/pkg/controller"
+	"github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/go-logr/logr"
 	"github.com/ironcore-dev/ironcore/api/common/v1alpha1"
