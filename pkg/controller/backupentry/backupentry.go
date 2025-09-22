8d95a9815b17bd17c0d11a1b4e41fc92de8558b8
Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>
2025-09-22 13:03:22 +0200

    remove s3ObjectLister interface
    The interface is no longer needed for mocking and the functionality is
    integrated into the DeleteObjectsWithPrefix function.

--- pkg/controller/backupentry/backupentry.go
+++ pkg/controller/backupentry/backupentry.go
@@ -6,22 +6,20 @@ package backupentry
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
-type s3ObjectLister interface {
-	ListObjectsPages(ctx context.Context, s3Client S3Client, input *s3.ListObjectsV2Input, bucketName string) error
-}
+// DeleteObjectsWithPrefix deletes the s3 objects with the specific <prefix>
+// from <bucket>. If it does not exist, no error is returned.
+func DeleteObjectsWithPrefix(ctx context.Context, s3Client S3Client, bucketName, prefix string) error {
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+		Prefix: aws.String(prefix),
+	}
 
-type s3ObjectListerImpl struct{}
-
-var objectLister s3ObjectLister = s3ObjectListerImpl{}
-
-func (o s3ObjectListerImpl) ListObjectsPages(ctx context.Context, s3Client S3Client, input *s3.ListObjectsV2Input, bucketName string) error {
 	paginator := s3.NewListObjectsV2Paginator(s3Client, input)
 	for paginator.HasMorePages() {
 		objectIDs := make([]s3types.ObjectIdentifier, 0)
@@ -48,20 +46,6 @@ func (o s3ObjectListerImpl) ListObjectsPages(ctx context.Context, s3Client S3Cli
 				return err
 			}
 		}
-	}
-	return nil
-}
-
-// DeleteObjectsWithPrefix deletes the s3 objects with the specific <prefix>
-// from <bucket>. If it does not exist, no error is returned.
-func DeleteObjectsWithPrefix(ctx context.Context, s3Client S3Client, bucketName, prefix string) error {
-	in := &s3.ListObjectsV2Input{
-		Bucket: aws.String(bucketName),
-		Prefix: aws.String(prefix),
-	}
-
-	if err := objectLister.ListObjectsPages(ctx, s3Client, in, bucketName); err != nil {
-		return fmt.Errorf("failed to list objects pages: %w", err)
 	}
 
 	return nil
