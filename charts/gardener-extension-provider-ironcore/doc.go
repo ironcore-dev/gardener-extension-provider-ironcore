c2073ae3bdb0728fb7f1746dccffd1a8fe5c93e1
Felix Riegger <felix.riegger@sap.com>
2026-09-11 13:10:50 +0200

    Remove year from license headers (#919)
    * Remove year from license headers
    
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>
    
    * Add generation diff caused by doc.go
    
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>
    
    ---------
    
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- charts/gardener-extension-provider-ironcore/doc.go
+++ charts/gardener-extension-provider-ironcore/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
 //go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh provider-ironcore . $(cat ../../VERSION) ../../example/controller-registration.yaml BackupBucket:ironcore BackupEntry:ironcore Bastion:ironcore ControlPlane:ironcore Infrastructure:ironcore Worker:ironcore"
--- example/doc.go
+++ example/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
 //go:generate sh -c "bash $GARDENER_HACK_DIR/generate-crds.sh -p 20-crd- extensions.gardener.cloud resources.gardener.cloud"
